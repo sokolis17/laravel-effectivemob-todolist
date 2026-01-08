a36d3cc1cfed74272123a9481552be50c7a71d62
Igor <rononi@bk.ru>
2026-01-08 22:50:17 +0300

    Создал файл api.php и прописал все свои Route

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+
+Route::post('/tasks',[TaskController::class,'store']);
+Route::get('/tasks',[TaskController::class,'index']);
+Route::get('/tasks/{id}',[TaskController::class,'show']);
+Route::put('/tasks/{id}',[TaskController::class,'update']);
+Route::delete('/tasks/{id}',[TaskController::class,'destroy']);
+
+
+#Route::apiResource('tasks', TaskController::class);
